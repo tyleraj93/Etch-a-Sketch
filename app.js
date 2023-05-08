39938f34904ddf3218c97622868c6ea48b827eed
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-05-08 19:41:43 -0400

    Change size of grid, start functinality for grid size

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ container.classList.add("container");
 document.body.appendChild(container);
 
 const get_random_rgb = () => (Math.floor(Math.random() * 256));
+const grid_size = get_grid_size();
 
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
+// Made funtction to get grid size.
+// Start with making a button to use function on click
+function get_grid_size() {
+    num = parseInt(prompt("What size would you like the grid (Smaller than 50x50)?"));
+    if (num > 50) {
+        get_grid_size();
+    }
+    return num;
+}
+
+for (let i = 0; i < grid_size; i++) {
+    for (let i = 0; i < grid_size; i++) {
         const div = document.createElement("div");
         div.classList.add("grid");
-        const size = container.clientHeight / 16;
+        const size = container.clientHeight / grid_size;
         div.style.height = `${size}px`;
         div.style.width = `${size}px`;
         container.appendChild(div);
