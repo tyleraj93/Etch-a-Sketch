dc9b36b95e8a2975d807456d27284b05b26e6fb8
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-05-08 19:24:42 -0400

    Add functionality to hover to make random color

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hello, world")
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+const get_random_rgb = () => (Math.floor(Math.random() * 256));
+
+for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid");
+        const size = container.clientHeight / 16;
+        div.style.height = `${size}px`;
+        div.style.width = `${size}px`;
+        container.appendChild(div);
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = `rgb(${get_random_rgb()}, ${get_random_rgb()}, ${get_random_rgb()})`;
+        });
+    };
+};
