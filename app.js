cb8f3de0d30e3a12592cfa58b0aee9825a0faa4d
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-05-09 17:03:26 -0400

    Add button to change size and reset sketch area

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-const container = document.createElement("div");
-container.classList.add("container");
-document.body.appendChild(container);
+const grid_size = document.createElement("button");
+grid_size.textContent = "Change size";
+document.body.appendChild(grid_size);
+const grid_container = document.createElement("div");
+grid_container.classList.add("grid_container");
+document.body.appendChild(grid_container);
 
-const get_random_rgb = () => (Math.floor(Math.random() * 256));
-const grid_size = get_grid_size();
+const get_random_rgb = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const get_shade_black = () => {
+    const l = Math.floor(Math.random() * 101);
+}
 
 // Made funtction to get grid size.
 // Start with making a button to use function on click
 function get_grid_size() {
-    num = parseInt(prompt("What size would you like the grid (Smaller than 50x50)?"));
-    if (num > 50) {
+    num = parseInt(
+        prompt("What size would you like the grid (Smaller than 100)?")
+    );
+    if (num > 100) {
         get_grid_size();
     }
     return num;
 }
 
-for (let i = 0; i < grid_size; i++) {
+function make_grid(grid_size) {
+    const size = grid_container.clientHeight / grid_size;
     for (let i = 0; i < grid_size; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid");
-        const size = container.clientHeight / grid_size;
-        div.style.height = `${size}px`;
-        div.style.width = `${size}px`;
-        container.appendChild(div);
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = `rgb(${get_random_rgb()}, ${get_random_rgb()}, ${get_random_rgb()})`;
-        });
-    };
-};
+        for (let i = 0; i < grid_size; i++) {
+            let div = document.createElement("div");
+            div.classList.add("grid");
+            div.style.height = `${size}px`;
+            div.style.width = `${size}px`;
+            grid_container.appendChild(div);
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = `${get_random_rgb()}`;
+            });
+        }
+    }
+}
+
+grid_size.addEventListener("click", () => {
+    const grids_to_remove = document.querySelectorAll(".grid");
+    grids_to_remove.forEach((grid) => {
+        grid.remove();
+    });
+    make_grid(get_grid_size());
+});
